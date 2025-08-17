ab62ffb3c17674ae97df763cb67ab05d61192a9e
“Idan <idanday2@gmail.com>
2025-08-17 18:55:36 +0300

    asd

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -s http://localhost:3000/health | grep "Still working" && echo "PASS ✅" || echo "FAIL ❌2"
+curl -s http://localhost:3000/health | grep "Still working" && echo "PASS ✅" || echo "FAIL ❌5"
