85cb98aaaf0baad3a9e3b46cbdececd6f4581252
“Idan <idanday2@gmail.com>
2025-08-17 14:12:46 +0300

    change port to 3006

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.get('/health', (req, res) => res.send('Still working... on *my* machine 🧃'));
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3006;
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
