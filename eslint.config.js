0a059a5594409b80c86db2f9ee89b26755d0ca70
“Idan <idanday2@gmail.com>
2025-08-17 18:21:24 +0300

    fix linter and server.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,8 @@
+import globals from "globals";
 import js from "@eslint/js";
 
 export default [
+    {languageOptions: { globals: globals.node } },
   js.configs.recommended,
   {
     // Your custom configurations or overrides if neeeded. 
--- src/server.js
+++ src/server.js
@@ -1,3 +1,7 @@
+// import express from 'express';
+// import process from 'process';
+// import console from 'console';
+
 const express = require('express');
 const app = express();
 
